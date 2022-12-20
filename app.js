94341f7c44b62070578599e685a017a31d869e50
scottCheung <scottcx2@gmail.com>
2022-12-20 00:33:20 -0500

    bug in carousel is conflicting with the comment post on blog page

--- app.js
+++ app.js
@@ -6,15 +6,70 @@ navToggle.addEventListener('click', function() {
  mainNav.classList.toggle('mobileNav')
 })
 
-//----------add comment function------------
-const checkPage = () => {
-    if(title !== 'Willow'){
-        return;
-    } else {
-        //select form
-const comment = document.querySelector('.blogForm');
+// ---------- Carousel -----------------------
+function carousel() {
+//Get the element with nodes that will be used for th carousel
+			imgList = document.querySelectorAll('.imageSlide');
+			console.log(imgList);
 
-//add eventlistener to form on submit
+			//add the translateX property by looping through the node list and multiplying each index by 100 to assign it's translateX value. This places all the images to the right of each other.
+
+			imgList.forEach((imageSlide, indx) => {
+				imageSlide.style.transform = `translateX(${indx * 100}%)`;
+			});
+
+			//visible Image counter
+			let visibleImage = 0;
+
+			//use selector for the "next" and "prev" buttons
+			const next = document.getElementById('nextImage');
+			
+			console.log(next);
+			//set a max counter
+			let totalImages = imgList.length - 1;
+
+			//write the functionality for the "Next" button
+			next.addEventListener('click', function() {
+
+				if (visibleImage === totalImages) {
+					visibleImage = 0;
+				} else {
+					visibleImage++;
+				};
+
+				imgList.forEach((imageSlide, indx) => {
+					imageSlide.style.transform = `translateX(${100 * (indx - visibleImage)}%)`;
+				});
+
+			});
+
+            const prev = document.getElementById('prevImage');
+
+			//write the functionality for the "Prev" button
+			prev.addEventListener('click', function () {
+
+				if (visibleImage === 0) {
+					visibleImage = totalImages;
+				} else {
+					visibleImage--;
+				}
+
+				imgList.forEach((imageSlide, indx) => {
+					imageSlide.style.transform = `translateX(${100 * (indx - visibleImage)}%)`;
+
+				});
+			});
+
+}
+			
+
+            //----------add comment function------------
+
+
+// function commentForm() {
+  comment = document.querySelector('.blogForm');
+    // console.log(comment);
+    //add eventlistener to form on submit
     comment.addEventListener('submit', function(event) {
         event.preventDefault();
 
@@ -24,100 +79,21 @@ const comment = document.querySelector('.blogForm');
         //select the place to insert a new post
         const newPost = document.querySelector('.blogMain');
 
-        //insert new post content with html
-        newPost.insertAdjacentHTML( 'beforeend',`<article class="post1">
-                                <div class="imageUserPost">
-                                    <img src="./assets/user-solid.svg" alt="icon of a picture">
-                                </div>
-                                <article>
-                                    <p>${new Date().toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"long", day:"numeric"})} by ${nameInput} 
-                                    <br>
-                                    <br>
-                                    ${commentInput}</p>
-                                </article>
-                            </article>`);
+    //insert new post content with html
+    newPost.insertAdjacentHTML('beforeend', `<article class="post1">
+                            <div class="imageUserPost">
+                                <img src="./assets/user-solid.svg" alt="icon of a picture">
+                            </div>
+                            <article>
+                                <p>${new Date().toLocaleDateString('en-us', { weekday: "long", year: "numeric", month: "long", day: "numeric" })} by ${nameInput} 
+                                <br>
+                                <br>
+                                ${commentInput}</p>
+                            </article>
+                        </article>`);
 
         //reset form after submit
         document.querySelector('.blogForm').reset();
-});
-    }
-}
-
-
-// --------- index Slide Show -----------------------
-    //get photos
-    // const slideImg = document.querySelectorAll('.slidePhoto');
-
-    //Loop through the photo's, show one at a time, hide the others
-    // let i = 0;
-
-    // setInterval (function() {
-               
-    // if(i == 0) {
-    //     slideImg[i].style.display = 'block';
-    // }else if (i === slideImg.length){
-    //     slideImg[i - 1].style.display = 'none';
-    //     slideImg[0].style.display = 'block';
-    //     i = 0;
-    // }else {
-    //     slideImg[i - 1].style.display = 'none';
-    //     slideImg[i].style.display = 'block';
-    // };
-
-    //     i++;
-
-    // }, 2000)
-
-    //Use css to transition
-
-// ---------- Carousel -----------------------
-
-//Get the element with nodes that will be used for th carousel
-    const imgList = document.querySelectorAll('.imageSlide');
-    console.log(imgList);
-
-//add the translateX property by looping through the node list and multiplying each index by 100 to assign it's translateX value. This places all the images to the right of each other.
-    imgList.forEach((imageSlide, indx) => {
-        imageSlide.style.transform = `translateX(${indx * 100}%)`;
-    })
-
-    //visible Image counter
-    let visibleImage = 0;
-
-    //use selector for the "next" and "prev" buttons
-    const next = document.querySelector('.nextImage');
-    const prev = document.querySelector('.prevImage');
-
-    //set a max counter
-    let totalImages = imgList.length - 1;
-
-    // write the functionality for the "Next" button
-    next.addEventListener('click', function() {
-        
-        if(visibleImage === totalImages){
-            visibleImage = 0;
-        }else {
-            visibleImage++;
-        };
-
-            imgList.forEach((imageSlide, indx) => {
-            imageSlide.style.transform = `translateX(${100 * (indx - visibleImage)}%)`;
-        });
-        
     });
-
-    // write the functionality for the "Prev" button
-    prev.addEventListener('click', function () {
-       
-        if(visibleImage === 0) {
-            visibleImage = totalImages;
-        } else {
-            visibleImage--;
-        }
-        
-
-        imgList.forEach((imageSlide, indx) => {
-            imageSlide.style.transform = `translateX(${100 * (indx - visibleImage)}%)`;
-        
-        });
-    });
+  
+// }
