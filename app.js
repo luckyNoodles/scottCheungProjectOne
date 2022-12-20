8751310f1a891560ea27a6ab80e817dd178e1e32
scottCheung <scottcx2@gmail.com>
2022-12-20 01:39:44 -0500

    added conditions to prevent null values from other page loads from blocking script functions

--- app.js
+++ app.js
@@ -9,68 +9,72 @@ navToggle.addEventListener('click', function() {
 // ---------- Carousel -----------------------
 function carousel() {
 //Get the element with nodes that will be used for th carousel
-			imgList = document.querySelectorAll('.imageSlide');
-			console.log(imgList);
+	imgList = document.querySelectorAll('.imageSlide');
+	console.log(imgList);
 
-			//add the translateX property by looping through the node list and multiplying each index by 100 to assign it's translateX value. This places all the images to the right of each other.
+	//add the translateX property by looping through the node list and multiplying each index by 100 to assign it's translateX value. This places all the images to the right of each other.
 
-			imgList.forEach((imageSlide, indx) => {
-				imageSlide.style.transform = `translateX(${indx * 100}%)`;
-			});
+	imgList.forEach((imageSlide, indx) => {
+		imageSlide.style.transform = `translateX(${indx * 100}%)`;
+	});
 
-			//visible Image counter
-			let visibleImage = 0;
+	//visible Image counter
+	let visibleImage = 0;
 
-			//use selector for the "next" and "prev" buttons
-			const next = document.getElementById('nextImage');
-			
-			console.log(next);
-			//set a max counter
-			let totalImages = imgList.length - 1;
+	//use selector for the "next" and "prev" buttons
+	const next = document.getElementById('nextImage');
+	
+	console.log(next);
+	//set a max counter
+	let totalImages = imgList.length - 1;
 
-			//write the functionality for the "Next" button
-			next.addEventListener('click', function() {
+	//write the functionality for the "Next" button
+	if(next !== null){
+		next.addEventListener('click', function() {
 
-				if (visibleImage === totalImages) {
-					visibleImage = 0;
-				} else {
-					visibleImage++;
-				};
+		if (visibleImage === totalImages) {
+			visibleImage = 0;
+		} else {
+			visibleImage++;
+		};
 
-				imgList.forEach((imageSlide, indx) => {
-					imageSlide.style.transform = `translateX(${100 * (indx - visibleImage)}%)`;
-				});
+		imgList.forEach((imageSlide, indx) => {
+			imageSlide.style.transform = `translateX(${100 * (indx - visibleImage)}%)`;
+		});
 
-			});
+	});
+	}
+		
+	const prev = document.getElementById('prevImage');
 
-            const prev = document.getElementById('prevImage');
+	//write the functionality for the "Prev" button
+	if(prev !== null){
+		prev.addEventListener('click', function () {
 
-			//write the functionality for the "Prev" button
-			prev.addEventListener('click', function () {
+		if (visibleImage === 0) {
+			visibleImage = totalImages;
+		} else {
+			visibleImage--;
+		}
 
-				if (visibleImage === 0) {
-					visibleImage = totalImages;
-				} else {
-					visibleImage--;
-				}
+		imgList.forEach((imageSlide, indx) => {
+			imageSlide.style.transform = `translateX(${100 * (indx - visibleImage)}%)`;
 
-				imgList.forEach((imageSlide, indx) => {
-					imageSlide.style.transform = `translateX(${100 * (indx - visibleImage)}%)`;
-
-				});
-			});
+		});
+	});
+	}
 
 }
-			
+carousel();
 
-            //----------add comment function------------
+//----------add comment function------------
 
-
-// function commentForm() {
+function commentForm() {
   comment = document.querySelector('.blogForm');
     // console.log(comment);
     //add eventlistener to form on submit
-    comment.addEventListener('submit', function(event) {
+	if(comment !== null){
+		comment.addEventListener('submit', function(event) {
         event.preventDefault();
 
         const nameInput = document.getElementById('name').value;
@@ -95,5 +99,7 @@ function carousel() {
         //reset form after submit
         document.querySelector('.blogForm').reset();
     });
-  
-// }
+	}
+}
+
+commentForm();
